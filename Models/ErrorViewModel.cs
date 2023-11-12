49f6dbc951f349fd0c4589a335930eede7844470
Marcel Salvenmoser <18032233+malthee@users.noreply.github.com>
2023-11-13 00:39:01 +0100

    Setup project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace patter_pal.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
